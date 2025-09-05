46d19f35101dad42bfecff27049de2cdd42c6749
binsey012 <domingo14vincemb@gmail.com>
2025-09-06 03:45:31 +0800

    build(pages): use BASE_PATH base in vite.config.ts and %BASE_URL% in index.html for favicon and entry script

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // For GitHub Pages deployments under a repo path
+  base: process.env.BASE_PATH || '/',
   plugins: [react()],
 })
